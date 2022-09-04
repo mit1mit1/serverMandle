9bf177793dc23135f8de58ebcb074913967bf28a
Mitchell (Midly) Roberts <mitchell.roberts11@gmail.com>
2022-09-04 16:57:20 +1000

    fix: actually set gridDistance

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 var express = require("express");
 const cors = require("cors");
 var app = express();
-app.use(
-  cors({
-    origin: "*",
-    optionsSuccessStatus: 200, // For legacy browser support
-    methods: "GET, PATCH",
-    exposedHeaders: "access-control-allow-origin",
-  })
-);
+app.use(cors());
 
 const playerPositions = [
   {
@@ -29,20 +22,18 @@ app.patch("/getPosition/:id", function (req, res) {
   } else {
     res.end(JSON.stringify(playerPositions[0]));
   }
-  console.log("GOTTEN");
 });
 
-app.patch("/setPosition/:id/:x/:y/:gridDistance", function (req, res) {
+app.patch("/setPosition/:id/:x/:y/:distance", function (req, res) {
   if (req.params.id < playerPositions.length && req.params.id >= 0) {
     playerPositions[req.params.id].x = req.params.x;
     playerPositions[req.params.id].y = req.params.y;
-    playerPositions[req.params.id].y = req.params.gridDistance;
+    playerPositions[req.params.id].gridDistance = req.params.distance;
   } else {
     playerPositions[0].x = req.params.x;
     playerPositions[0].y = req.params.y;
-    playerPositions[0].y = req.params.gridDistance;
+    playerPositions[0].gridDistance = req.params.distance;
   }
-  console.log("PATCHED");
   res.end();
 });
 
