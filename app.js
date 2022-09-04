97a49dac9aa8c657aea669fbc27cc654f2984c19
Mitchell (Midly) Roberts <mitchell.roberts11@gmail.com>
2022-09-04 16:41:10 +1000

    feat: keep track of zoom too

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(
     origin: "*",
     optionsSuccessStatus: 200, // For legacy browser support
     methods: "GET, PATCH",
-    exposedHeaders: "access-control-allow-origin"
+    exposedHeaders: "access-control-allow-origin",
   })
 );
 
@@ -14,14 +14,16 @@ const playerPositions = [
   {
     x: 0,
     y: 0,
+    gridDistance: 1,
   },
   {
     x: 0,
     y: 0,
+    gridDistance: 1,
   },
 ];
 
-app.get("/getPosition/:id", function (req, res) {
+app.patch("/getPosition/:id", function (req, res) {
   if (req.params.id < playerPositions.length && req.params.id >= 0) {
     res.end(JSON.stringify(playerPositions[req.params.id]));
   } else {
@@ -30,13 +32,15 @@ app.get("/getPosition/:id", function (req, res) {
   console.log("GOTTEN");
 });
 
-app.patch("/setPosition/:id/:x/:y", function (req, res) {
+app.patch("/setPosition/:id/:x/:y/:gridDistance", function (req, res) {
   if (req.params.id < playerPositions.length && req.params.id >= 0) {
     playerPositions[req.params.id].x = req.params.x;
     playerPositions[req.params.id].y = req.params.y;
+    playerPositions[req.params.id].y = req.params.gridDistance;
   } else {
     playerPositions[0].x = req.params.x;
     playerPositions[0].y = req.params.y;
+    playerPositions[0].y = req.params.gridDistance;
   }
   console.log("PATCHED");
   res.end();
